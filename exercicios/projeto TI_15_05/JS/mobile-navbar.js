99917656718d9808089df696ccad30c793127162
Kaua1007 <kaua.mascarenhas107@gmail.com>
2025-06-02 16:27:43 -0300

    .

--- exercicios/projeto TI_15_05/JS/mobile-navbar.js
+++
@@ -1,9 +0,0 @@
-var imgAtual = "./imagens/Carlo Ancelotti.png"
-var imagemAnterior = "./imagens/senac.png"
-
-function trocar () {
-    document.getElementById('troca').scr = imgAtual;
-    let aux = imgAtual
-    imgAtual = imagemAnterior
-    imagemAnterior = aux
-}
